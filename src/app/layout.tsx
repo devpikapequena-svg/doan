dc8c154e64f3dcf8d5d13420adee0e993878511b
devpikapequena-svg <devpikapequena@gmail.com>
2026-01-04 18:31:38 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import { CartProvider } from '@/context/CartContext'
 import { PaymentProvider } from '@/context/PaymentContext'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
-const META_PIXEL_ID = ''
+const TIKTOK_PIXEL_ID = 'D5DDL7RC77UCQLCHJ3KG' // Replace with your actual TikTok Pixel ID
 
 function LayoutContent({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
@@ -50,8 +50,8 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (isNotFoundPage) return
-    if (typeof window !== 'undefined' && typeof (window as any).fbq === 'function') {
-      ;(window as any).fbq('track', 'PageView')
+    if (typeof window !== 'undefined' && typeof (window as any).ttq === 'function') {
+      ;(window as any).ttq.page()
     }
   }, [pathname, searchParams, isNotFoundPage])
 
@@ -111,18 +111,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         />
 
         {/* ================================
-            Meta Pixel
+            TikTok Pixel
         ================================= */}
-        <Script id="meta-pixel" strategy="afterInteractive">
+        <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${META_PIXEL_ID}');
-            fbq('track', 'PageView');
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+              var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+              ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+              ttq.load('${TIKTOK_PIXEL_ID}');
+              ttq.page();
+            }(window, document, 'ttq');
           `}
         </Script>
-
       </head>
 
       <body className={cn('font-sans', inter.variable)}>
@@ -131,7 +132,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            src={`https://www.tiktok.com/tr?id=${TIKTOK_PIXEL_ID}&ev=PageView&noscript=1`}
             alt=""
           />
         </noscript>
